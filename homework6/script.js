fbe1e42871f4d33f5dd1a62b2885fd941ca6f4a4
nadzeyasukhikh <129493288+nadzeyasukhikh@users.noreply.github.com>
2023-04-25 18:35:04 +0300

    Add files via upload
    homework6

---
+++ homework6/script.js
@@ -0,0 +1,31 @@
+//  const numbers = [10, 0, 70, 1000, 95]
+//   const names = ["Nadia", "Artur", "Maria"]
+//  console.log(numbers[3])
+
+
+//   for(let i = 0; i < 11; i+=2) {
+//       console.log(i)
+//   }
+
+// for(let i = 55; i > 20; i--) {
+//     console.log(i)
+// }
+
+
+//   const numbers = [3, 5, 11, 2, 8, 1, 6];
+//   const numbers_squared = []
+//   for(let i = 0; i < numbers.length; i++) {
+//      console.log(numbers[i])
+//      const newNumber = numbers[i] **2
+//      newNumber.push(numbers_squared)
+//   }
+// let last_elem = numbers_squared[6]
+
+const user = {
+    first_name: 'Ivan', 
+    last_name: 'Ivanov', 
+    age: 20, 
+    salary: 500
+    }
+    console.log(`User’s name is ${user.first_name} ${user.last_name}.
+     He is ${user.age} years old.`)
