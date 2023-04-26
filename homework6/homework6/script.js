45a1db2de232efba31f9b76eab2e6abd8767917b
nadzeyasukhikh <129493288+nadzeyasukhikh@users.noreply.github.com>
2023-04-26 22:16:18 +0300

    Add files via upload

--- homework6/homework6/script.js
+++ homework6/homework6/script.js
@@ -8,20 +8,21 @@
 // }
 
 
-//   const numbers = [3, 5, 11, 2, 8, 1, 6];
-//   const numbers_squared = []
-//   for(let i = 0; i < numbers.length; i++) {
+//    const numbers = [3, 5, 11, 2, 8, 1, 6];
+//    const numbers_squared = []
+//    for(let i = 0; i < numbers.length; i++) {
 //      console.log(numbers[i])
+//             }
 //      const newNumber = numbers[i] **2
 //      newNumber.push(numbers_squared)
-//   }
+//   
 // let last_elem = numbers_squared[6]
 
-const user = {
-    first_name: 'Ivan', 
-    last_name: 'Ivanov', 
-    age: 20, 
-    salary: 500
-    }
-    console.log(`User’s name is ${user.first_name} ${user.last_name}.
-     He is ${user.age} years old.`)
+// const user = {
+//     first_name: 'Ivan', 
+//     last_name: 'Ivanov', 
+//     age: 20, 
+//     salary: 500
+//     }
+//     console.log(`User’s name is ${user.first_name} ${user.last_name}.
+//      He is ${user.age} years old.`)
