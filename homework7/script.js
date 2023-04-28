77de33ff22b87637c118c4b8f31d742285ec76dd
nadzeyasukhikh <129493288+nadzeyasukhikh@users.noreply.github.com>
2023-04-28 23:45:33 +0300

    Add files via upload

--- homework7/script.js
+++ homework7/script.js
@@ -1,26 +1,37 @@
 // Напишите функцию, которая принимает два числовых аргумента
 // и выводит в консоль все четные числа от большего к меньшему.
 
-// function getArrey(number1, number2) {
-//   const numbers = [];
-//   if (number1 < number2) {
-//     for (let i = number1; i <= number2; i++) {
-//       numbers.push(i);
-//     }
-//   } else  {
-//     for (let i = number2; i <= number1; i++) {
-//       numbers.push(i);
-//     }
-//   }
+function getArrey(number1, number2) {
+     const numbers = [];
+     if (number1 > number2) {
+       for (let i = number1; i >= number2; i--) {
+         numbers.push(i);
+      }
+     } else  {
+      for (let i = number2; i >= number1; i--) {
+        numbers.push(i);
+      }
+     }
+  
+     for (let i = 0; i < numbers.length; i++) {
+       if (numbers[i] % 2 == 0) {
+         console.log(numbers[i]);
+       }
+  
+    }
+   }
+  getArrey(80, 90);
 
-//   for (let i = 0; i < numbers.length; i++) {
-//     if (numbers[i] % 2 == 0) {
-//       console.log(numbers[i]);
-//     }
+//  function printEvenNumbersDescending(num1, num2) {
+//   let start = Math.max(num1, num2);
+//   let end = Math.min(num1, num2);
 
+//   for (let i = start; i >= end; i--) {
+//     if (i % 2 == 0) {
+//       console.log(i);
+//     }
 //   }
 // }
-// console.log(getArrey(90, 80));
 
 // Напишите функцию power, которая принимает два числовых аргумента
 // (основание степени и саму степень) и возвращает число в указанной степени.
@@ -68,16 +79,16 @@
 //  функция возвращает null. Если есть несколько одинаковых по длине элементов -
 //  функция возвращает первый из этих элементов.
 
-function longestString(word) {
-  if (word.length == 0) {
-    return null;
-  }
-  let longest = word[0];
-  for (let i = 1; i < word.length; i++) {
-    if (word[i].length > longest.length) {
-      longest = word[i];
-    }
-  }
-  return longest;
-}
-console.log(longestString(["one", "two", "three"]));
+// function longestString(word) {
+//   if (word.length == 0) {
+//     return null;
+//   }
+//   let longest = word[0];
+//   for (let i = 1; i < word.length; i++) {
+//     if (word[i].length > longest.length) {
+//       longest = word[i];
+//     }
+//   }
+//   return longest;
+// }
+// longestString(["one", "two", "three"]);
