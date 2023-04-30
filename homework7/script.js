382dc161eb50b3aca23cba537947be7a77487533
nadzeyasukhikh <129493288+nadzeyasukhikh@users.noreply.github.com>
2023-04-30 22:33:53 +0300

    Add files via upload

--- homework7/script.js
+++ homework7/script.js
@@ -31,12 +31,12 @@
 // }
 // power(3)
 
-// Напишите функцию, которая принимает числовой аргумент n
-// и считает сумму чисел от 1 до n.
+//  Напишите функцию, которая принимает числовой аргумент n
+//  и считает сумму чисел от 1 до n.
 
-// function sumNumbers(n) {
-//   let sum = 0;
-//   for (let i = 1; i <= n; i++) {
+//  function sumNumbers(n) {
+//    let sum = 0;
+//    for (let i = 1; i <= n; i++) {
 //     sum += i;
 //   }
 //    return sum;
@@ -47,40 +47,62 @@
 //  и считает сумму четных чисел и нечетных чисел от n до m.
 //  Суммы выведите в консоль (в начале сумму четных чисел, а затем сумму нечетных).
 
-// function sumNumbers(n, m) {
-//   let sumEven = 0;
-//   let sumOdd = 0;
-//   for (let i = n; i <= m; i++) {
-//     if (i % 2 == 0) {
-//       sumEven += i;
-//     } else {
-//       sumOdd += i;
-//     }
-//   }
-//   console.log(sumEven);
-//   console.log(sumOdd);
-// }
+ function sumNumbers(number1, number2) {
+  let sumEven = 0;
+   let sumOdd = 0;
+   for (let i = number1; i <= number2; i++) {
+     if (i % 2 == 0) {
+       sumEven += i;
+     } else {
+       sumOdd += i;
+     }
+   }
+   for (let i = number2; i <= number1; i++) {
+    if (i % 2 == 0) {
+      sumEven += i;
+    } else {
+      sumOdd += i;
+    }
+  }
+  const numbers = []
+     if(number1 < number2) {
+        for(let i = number1; i <= number2; i++) {
+            numbers.push(i)
+        }
+    }
+     else {
+         for(let i = number2; i <= number1; i++) {
+             numbers.push(i)
+        }
+       
+    }
+  
+   
+  //  console.log(sumEven);
+   console.log(sumOdd);
+  
+ }
 
-// sumNumbers(1, 3);
+ sumNumbers(6, 1);
 
 // Напишите функцию, которая принимает в качестве аргументов массив строк,
 //  а возвращает первый самый длинный элемент массива. Если входной массив пуст,
 //  функция возвращает null. Если есть несколько одинаковых по длине элементов -
 //  функция возвращает первый из этих элементов.
 
- function longestString(word) {
-    if (word.length == 0) {
-      console.log(null);
-   }
-    let longest = word[0];
-  for (let i = 1; i < word.length; i++) {
-    if (word[i].length > longest.length) {
-            longest = word[i];
-      }
-    }
-    console.log(longest);
- }
-  longestString(["one", "two","erty","hgfj","oiuy" ])
+//  function longestString(word) {
+//     if (word.length == 0) {
+//       console.log(null);
+//    }
+//     let longest = word[0];
+//   for (let i = 1; i < word.length; i++) {
+//     if (word[i].length > longest.length) {
+//             longest = word[i];
+//       }
+//     }
+//     console.log(longest);
+//  }
+//   longestString(["one", "two","erty","hgfj","oiuy" ])
 
 
 
