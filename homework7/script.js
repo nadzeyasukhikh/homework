a1bfc94d0bbe06a3cea376b88cee117a577295a8
nadzeyasukhikh <129493288+nadzeyasukhikh@users.noreply.github.com>
2023-04-30 21:40:23 +0300

    Add files via upload

--- homework7/script.js
+++ homework7/script.js
@@ -1,28 +1,26 @@
 // Напишите функцию, которая принимает два числовых аргумента
 // и выводит в консоль все четные числа от большего к меньшему.
 
-function getArrey(number1, number2) {
-     const numbers = [];
-     if (number1 > number2) {
-       for (let i = number1; i >= number2; i--) {
-         numbers.push(i);
-      }
-     } else  {
-      for (let i = number2; i >= number1; i--) {
-        numbers.push(i);
-      }
-     }
-  
-     for (let i = 0; i < numbers.length; i++) {
-       if (numbers[i] % 2 == 0) {
-         console.log(numbers[i]);
-       }
-  
-    }
-   }
-  getArrey(80, 90);
+// function getArrey(number1, number2) {
+//      const numbers = [];
+//      if (number1 > number2) {
+//        for (let i = number1; i >= number2; i--) {
+//          numbers.push(i);
+//       }
+//      } else  {
+//       for (let i = number2; i >= number1; i--) {
+//         numbers.push(i);
+//       }
+//      }
 
+//      for (let i = 0; i < numbers.length; i++) {
+//        if (numbers[i] % 2 == 0) {
+//          console.log(numbers[i]);
+//        }
 
+//     }
+//    }
+//   getArrey(80, 90);
 
 // Напишите функцию power, которая принимает два числовых аргумента
 // (основание степени и саму степень) и возвращает число в указанной степени.
@@ -60,26 +58,29 @@ function getArrey(number1, number2) {
 //     }
 //   }
 //   console.log(sumEven);
-//    //console.log(sumOdd);
+//   console.log(sumOdd);
 // }
 
-//console.log(sumNumbers(6, 11))
+// sumNumbers(1, 3);
 
 // Напишите функцию, которая принимает в качестве аргументов массив строк,
 //  а возвращает первый самый длинный элемент массива. Если входной массив пуст,
 //  функция возвращает null. Если есть несколько одинаковых по длине элементов -
 //  функция возвращает первый из этих элементов.
 
-// function longestString(word) {
-//   if (word.length == 0) {
-//     return null;
-//   }
-//   let longest = word[0];
-//   for (let i = 1; i < word.length; i++) {
-//     if (word[i].length > longest.length) {
-//       longest = word[i];
-//     }
-//   }
-//   return longest;
-// }
-// longestString(["one", "two", "three"]);
+ function longestString(word) {
+    if (word.length == 0) {
+      console.log(null);
+   }
+    let longest = word[0];
+  for (let i = 1; i < word.length; i++) {
+    if (word[i].length > longest.length) {
+            longest = word[i];
+      }
+    }
+    console.log(longest);
+ }
+  longestString(["one", "two","erty","hgfj","oiuy" ])
+
+
+
